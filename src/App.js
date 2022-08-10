e3aa541fcd8149a3eac82713e1ad887186441bcd
Go Katsuki <go.katsuki@rakuten.com>
2022-08-10 16:03:06 +0900

    add header

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Page1 from "./components/Page1"
 const App = () => {
   return (
     <BrowserRouter>
+    <h1 className="bg-primary text-white display-4">React</h1>
       <Routes>
         <Route path={`/`} element={<Home />} />
         <Route path={`/page1/`} element={<Page1 />} />
