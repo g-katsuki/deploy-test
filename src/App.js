b5709fd390ec6475fe58bbac9986d95ce5cd090e
Go Katsuki <go.katsuki@rakuten.com>
2022-08-10 15:16:35 +0900

    unused componene test

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import './App.css'
+
+class App extends Component {
+  render(){
+    return  <div>
+      <h1 className="bg-primary text-white display-4">React</h1>
+      <h2>deploy</h2>
+    </div>
+  }
+}
+
+export default App
