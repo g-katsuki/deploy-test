98597b2547c42be4e84efd5c1be80a40aa3c2afd
Go Katsuki <go.katsuki@rakuten.com>
2022-08-15 20:55:58 +0900

    add sample json data

--- src/components/ApiFetch.js
+++ src/components/ApiFetch.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 
 export const ApiFetch = () => {
-	const [str, setStr] = useState([]);
+	const [data, setData] = useState({message:'', data:[]})
 
 	useEffect(() => {
 		// APIをfetchする(呼び出す)
@@ -10,14 +10,27 @@ export const ApiFetch = () => {
 			 .then((res) => res.json())
 			 //APIから渡されるレスポンスデータ(data)をstateにセットする
 			.then((data) => {
-				setStr(data);
+				setData(data);
 			});
 	}, []);
 
 	return (
-		<div>
-		    {}
-			<ul>{str.message}</ul>
-		</div>
+		<div className="alert alert-primary text-center">
+        <h5 className="mb-4">{data.message}</h5>
+        <table className="table bg-white">
+          <thead className="table-dark">
+            <tr><th>Name</th><th>Mail</th><th>Age</th></tr>
+          </thead>
+          <tbody>
+            {data.data.map((value, key)=> (
+              <tr key={key}>
+                <th>{value.name}</th>
+                <td>{value.mail}</td>
+                <td>{value.age}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
 	);
 };
