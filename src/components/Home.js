914f1b0e7bc67d24cd3731c29ad117e5ead3e564
Go Katsuki <go.katsuki@rakuten.com>
2022-08-15 20:33:48 +0900

    add json

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import { ApiFetch } from "./ApiFetch";
 
 const Home = () => {
   return (
@@ -7,6 +8,7 @@ const Home = () => {
       <div>
         <Link to={`/page1/`}>こちら</Link>
       </div>
+      <ApiFetch />
     </>
   );
 };
