b5bdab5ac5cf93a45ed151d846dd0d78fe7468fe
Go Katsuki <go.katsuki@rakuten.com>
2022-08-17 21:34:22 +0900

    change link design

--- src/components/Page1.js
+++ src/components/Page1.js
@@ -10,9 +10,9 @@ const Page1 = () => {
       <h1>ページ</h1>
         <Link to={`/`}>ホームに戻る</Link>
         <Tes />
-        <a className="bg-dark text-white display-4" href="https://kumamate.net/vip/">Link</a>
+        <a className="h4 bg-info text-white display-5 p-2 m-5" href="https://kumamate.net/vip/">Link</a>
     </div>
   );
 };
 
-export default Page1;
+export default Page1
