e4823d729b44b4b73a30cb3fe90f430ce39c4766
Go Katsuki <go.katsuki@rakuten.com>
2022-08-17 12:02:16 +0900

    add memo

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
+import MemoPage from './memo/MemoPage'
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <MemoPage />
   </React.StrictMode>,
   document.getElementById('root')
 )
